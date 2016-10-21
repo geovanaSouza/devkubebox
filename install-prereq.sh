15161bcfcc0ceaca7b4d5fa879a93c92ba52393f
geovana.souza <geovana.souza@bravi.com.br>
2016-10-21 17:29:57 -0200

    Initial

---
+++ install-prereq.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+kubectl_version=v1.4.3
+vagrant_box=coreos-stable
+
+install_kubectl(){
+  if [ "$(uname)" == "Darwin" ]; then
+    platform=darwin
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    platform=linux
+  elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    echo "Unsupported Platform"
+    exit 1
+  fi
+
+  if [ ! -f "/usr/local/bin/kubectl"  ];then
+    curl -O https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/${platform}/amd64/kubectl
+    chmod +x kubectl
+    sudo mv kubectl /usr/local/bin/kubectl
+  fi
+}
+
+update_vagrant_box(){
+  if [ $(vagrant box list  | grep coreos-stable) ]; then
+    echo "Update ${vagrant_box}"
+    vagrant box update --box ${vagrant_box}
+  fi
+}
+
+install_kubectl
+update_vagrant_box
